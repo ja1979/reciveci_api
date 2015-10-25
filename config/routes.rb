83fd7ea7a60eb83349948d06cdc50f115c8559fb
Jorge Aguilar <jorge.aguilar.ec@gmail.com>
2015-10-25 13:02:17 -0500

    Bug: HTML format by default instead of JSON

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  #resources :articles
-  resources :articles, :defaults => { :format => :json }
+  resources :articles
+  #resources :articles, :defaults => { :format => :json }
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
