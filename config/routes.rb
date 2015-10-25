a1148263a1da0120974ccee3e9e2939713a9dbe3
Jorge Aguilar <jorge.aguilar.ec@gmail.com>
2015-10-25 12:56:07 -0500

    Initial version of Articles(News)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  #resources :articles
+  resources :articles, :defaults => { :format => :json }
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
