54b10f3c6948da1790c5df4f0ac09349147d7505
Jorge Aguilar <jorge.aguilar.ec@gmail.com>
2015-11-01 22:31:12 -0500

    Waste picker image by default

--- app/models/waste_picker.rb
+++ app/models/waste_picker.rb
@@ -5,11 +5,18 @@ class WastePicker < ActiveRecord::Base
 
 	def image_url
 
+		image_base_path = "assets/images/waste_pickers/"
+
+
 		current_path = Dir.pwd		
 		base_path = current_path + "/public/"
-		image_path = "assets/images/waste_pickers/#{id}.jpg"
+		image_path = image_base_path + "#{id}.jpg"
 
-		image_path if File.exist?(base_path + image_path)
+		if File.exist?(base_path + image_path) then
+			image_path
+		else
+			image_base_path + "0.jpg"
+		end
 	end
 
 end
