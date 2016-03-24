18d59997266735504e0db4759438c9f965015d7c
Vagrant <vagrant@dev.reciveci.ec>
2016-03-24 09:33:46 -0500

    Agregar imagenes articulos

--- Gemfile
+++ Gemfile
@@ -22,10 +22,6 @@ group :development, :test do
   gem 'thor'
 end
 
-# Add support for the MySQL
-group :production, :mysql do
-  gem 'mysql2'
-end
 
 group :production, :postgresql do
   gem 'pg'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160304021652) do
+ActiveRecord::Schema.define(version: 20160323183039) do
 
   create_table "affiliations", force: true do |t|
     t.string   "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20160304021652) do
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "extension"
   end
 
   create_table "categories", force: true do |t|
