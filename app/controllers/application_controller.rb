61bcc72c8b2864f186ca03dfe7880305de3c8456
Jefferson <morochoji5@hotmail.com>
2016-03-30 15:22:27 -0500

    asignacion de roles a formulario de registro

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,7 @@ class ApplicationController < ActionController::Base
   protected 
 
   def configure_permitted_parameters
-  	devise_parameter_sanitizer.for(:sing_up) {|u| u.permit(:username, :email, :password, :password_confirmation)}
-  	devise_parameter_sanitizer.for(:sing_in) {|u| u.permit(:username, :email, :password, :password_confirmation)}
+  	devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :role, :password, :password_confirmation)}
+    devise_parameter_sanitizer.for(:sign_in) {|u| u.permit(:username, :email, :password)}
   end
 end
