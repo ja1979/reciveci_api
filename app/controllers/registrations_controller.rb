89f6cc3cf6f1e63c7d301be2f06457fd9df1fbe5
Alejo <alejosanzcr7@gmail.com>
2016-04-08 00:05:18 -0500

    centrando imagen de articles y agregando bootstrap al modulo de login

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,18 +1,12 @@
 class RegistrationsController < Devise::RegistrationsController 
 
   def new
-
-    flash[:info] = 'Los registros aun no estan habilitados, pero porfavor vuelva pronto.'
+    #flash[:info] = 'Los registros aun no estan habilitados, pero porfavor vuelva pronto.'
     redirect_to new_user_session_path
   end
 
   def create
-    flash[:info] = 'Los registros aun no estan habilitados, pero porfavor vuelva pronto.'
-    
-
-    
-    
+    #flash[:info] = 'Los registros aun no estan habilitados, pero porfavor vuelva pronto.'   
     redirect_to new_user_session_path
-  end
-	
+  end	
 end
