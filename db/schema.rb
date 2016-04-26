5b1fe5dd47ed1ad410bd1af02e080783eb2d776e
jaop2316 <jessikita.ldu@gmail.com>
2016-04-26 13:54:02 -0500

    Solucion conflictos

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160413202739) do
-
+ActiveRecord::Schema.define(version: 20160425150838) do
 
   create_table "affiliations", force: true do |t|
     t.string   "name"
